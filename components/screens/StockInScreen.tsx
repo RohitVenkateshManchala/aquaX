b9f679027587cd19f84006cc1b4e96da5895dc37
Rohit Venkatesh <mrohitvenkatesh@gmail.com>
2024-12-30 21:08:29 +0530

    created the required screens

---
+++ components/screens/StockInScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const StockInScreen = () => {
+  return (
+    <View>
+      <Text>StockInScreen</Text>
+    </View>
+  )
+}
+
+export default StockInScreen
+
+const styles = StyleSheet.create({})
---
+++ components/screens/StockOutScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const StockOutScreen = () => {
+  return (
+    <View>
+      <Text>StockOutScreen</Text>
+    </View>
+  )
+}
+
+export default StockOutScreen
+
+const styles = StyleSheet.create({})
